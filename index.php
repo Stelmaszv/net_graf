4a779b9a14e15502134aa62f1dfb2d14a6d62a4a
Stelmaszv <stelmaszv@gmail.com>
2023-11-21 15:01:14 +0100

    add ProductsList

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 use App\DB\Connect;
+use App\Api\ProductsList;
 require('vendor/autoload.php');
 
 $connect = Connect::getInstance();
 $engine = $connect->getEngine();
+
+$view = null;
+
+switch ($_GET['action']) {
+    case "list":
+        $view = new ProductsList($engine);
+        break;
+    default:
+        echo "Invalid action.";
+}
+
+echo $view->action();
